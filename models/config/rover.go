edabd2c37652ab9329e9eac29f611442cdd5f413
wikeexDT <wikeexm@vip.qq.com>
2020-04-17 10:42:36 +0800

    add cmd local forward to python

--- models/config/rover.go
+++ models/config/rover.go
@@ -7,20 +7,12 @@ import (
 )
 
 type RoverConfig struct {
-	ServerIP		string		`json:"serverIp"`
-	ServerPort		int			`json:"serverPort"`
-	LocalPort		int			`json:"localPort"`
-	PackageLen		int			`json:"packageLen"`
-	Id				int			`json:"id"`
-	GroupId			int			`json:"groupId"`
+	CmdServicePort		int		`json:"cmdServicePort"`
 }
 
 func GetDefaultRoverConfig() RoverConfig {
 	return RoverConfig{
-		ServerIP:   "140.143.99.31",
-		ServerPort: 10006,
-		LocalPort:  18000,
-		PackageLen: 548,
+		CmdServicePort: 10008,
 	}
 }
 
@@ -38,52 +30,12 @@ func RoverConfigInit(filePath string) (roverConfig RoverConfig, err error) {
 		value			int
 	)
 
-	if tempString, ok = conf.Get("common", "serverIp"); ok {
-		roverConfig.ServerIP = tempString
-	}
-
-	if tempString, ok = conf.Get("common", "serverPort"); ok {
+	if tempString, ok = conf.Get("common", "cmdServicePort"); ok {
 		value, err = strconv.Atoi(tempString)
 		if err != nil {
 			return
 		}
-		roverConfig.ServerPort = value
-	}
-
-	if tempString, ok = conf.Get("common", "localPort"); ok {
-		value, err = strconv.Atoi(tempString)
-		if err != nil {
-			return
-		}
-		roverConfig.LocalPort = value
-	}
-
-	if tempString, ok = conf.Get("common", "packageLen"); ok {
-		value, err = strconv.Atoi(tempString)
-		if err != nil {
-			return
-		}
-		roverConfig.PackageLen = value
-	}
-
-	if tempString, ok = conf.Get("common", "id"); ok {
-		value, err = strconv.Atoi(tempString)
-		if err != nil {
-			return
-		}
-		roverConfig.Id = value
-	} else {
-		panic("'ControllerId' variable missing from 'common' section")
-	}
-
-	if tempString, ok = conf.Get("common", "groupId"); ok {
-		value, err = strconv.Atoi(tempString)
-		if err != nil {
-			return
-		}
-		roverConfig.GroupId = value
-	} else {
-		panic("'groupId' variable missing from 'common' section")
+		roverConfig.CmdServicePort = value
 	}
 
 	return
--- rover/service.go
+++ rover/service.go
@@ -6,11 +6,12 @@ import (
 	"HomeRover/models/config"
 	"HomeRover/models/data"
 	"fmt"
+	"net"
 )
 
-func NewService(conf *config.CommonConfig, roverConf *config.RoverConfig, joystickData chan []byte) (service *Service, err error) {
+func NewService(conf *config.CommonConfig, roverConf *config.RoverConfig, joystickDataCh chan []byte) (service *Service, err error) {
 	service = &Service{
-		joystickData: 	joystickData,
+		joystickDataCh: 	joystickDataCh,
 	}
 
 	service.Conf = conf
@@ -24,7 +25,9 @@ type Service struct {
 	base.Service
 
 	roverConf 		*config.RoverConfig
-	joystickData	chan []byte
+	joystickDataCh	chan []byte
+
+	cmdServiceConn	*net.UDPConn
 }
 
 func (s *Service) cmdRecv()  {
@@ -54,7 +57,7 @@ func (s *Service) cmdRecv()  {
 				fmt.Println(err)
 			}
 			
-			s.joystickData <- recvEntity.Payload
+			s.joystickDataCh <- recvEntity.Payload
 			counter++
 			if counter == 255 {
 				sendEntity.GroupId = recvEntity.GroupId
@@ -70,6 +73,22 @@ func (s *Service) cmdRecv()  {
 	}
 }
 
-func cmdService()  {
-	
+func (s *Service) cmdService()  {
+	cmdServiceAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("127.0.0.1:%d", s.roverConf.CmdServicePort))
+	if err != nil {
+		fmt.Println(err)
+	}
+	sendAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
+	s.cmdServiceConn, err = net.ListenUDP("udp", sendAddr)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer s.cmdServiceConn.Close()
+
+	for {
+		_, err = s.cmdServiceConn.WriteToUDP(<- s.joystickDataCh, cmdServiceAddr)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 }
