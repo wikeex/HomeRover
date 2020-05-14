2aea31d5a300259fd8767b8aa76c8c9894ede7c8
wikeex <wikeexm@vip.qq.com>
2020-05-14 23:06:10 +0800

    add rover main

--- main/rover.go
+++ main/rover.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"HomeRover/models/config"
+	"HomeRover/rover"
+	"fmt"
+)
+
+func main()  {
+	conf, err := config.CommonConfigInit("conf/rover.ini")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	roverConf, err := config.RoverConfigInit("conf/rover.ini")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	service, err := rover.NewService(&conf, &roverConf)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	service.Run()
+}
