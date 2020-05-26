1e7722d42ba64f9d286fed6e828c5e88fdf0db79
wikeex <wikeexm@vip.qq.com>
2020-05-26 19:14:29 +0800

    adjust driver

--- driver/drive.py
+++ driver/drive.py
@@ -40,7 +40,7 @@ def drive():
         pwm.setServoPulse(RIGHT_MOTOR, right_motor)
 
         pwm.setRotationAngle(CAM_X, 160 - (right_x / 128.0 * 70 + 90))
-        pwm.setRotationAngle(CAM_Y, right_y / 128.0 * 70 + 180)
+        pwm.setRotationAngle(CAM_Y, right_y / 128.0 * 70 + 150)
 
 
 if __name__ == '__main__':
